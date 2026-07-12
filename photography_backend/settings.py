dd2d0a030ab835f5079bca7a384d108c17f99853
Melissa Whittington <melissawhittington975@gmail.com>
2026-07-12 16:54:29 -0700

    Fix BASE_DIR typo

--- photography_backend/settings.py
+++ photography_backend/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
-BASE_DIR = Path(file).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
